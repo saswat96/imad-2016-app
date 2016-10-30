e831c207dd543310cf80fb3450e37ffad9c95f43
Saswat Panigrahi <saswat96@gmail.com>
2016-10-30 16:01:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -156,8 +156,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res){
-    res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req, res){
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two', function (req, res){
