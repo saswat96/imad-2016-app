4e9dedc7c14ce819fdc5d9d214cf5804947ae68f
Saswat Panigrahi <saswat96@gmail.com>
2016-11-08 09:39:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,20 @@
 console.log('Loaded!');
 
 var button = document.getElementById("counter");
-var counter = 0; 
-button.onclick = function(){
+button.onclick = function() {
     
     
-    
-    
-    
-    
-    
-    
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
-    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = fucntion(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+             var counter = request.responseText;
+             var span = document.getElementById("count");
+             span.innerHTML = counter.toString();
+            }
+        }
+        
+    }
+    request.open('GET','http://saswat96.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
