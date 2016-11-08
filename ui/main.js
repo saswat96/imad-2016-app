e2b0f97c4371b735b84b20e0c56de7152c93f831
Saswat Panigrahi <saswat96@gmail.com>
2016-11-08 17:25:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,9 +8,7 @@ button.onclick = function() {
     request.onreadystatechange = function() {
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-             var counter = request.responseText;
-             var span = document.getElementById("count");
-             span.innerHTML = counter.toString();
+            
             }
         }
         
@@ -24,11 +22,18 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_button');
 submit.onclick = function(){
     
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i= 0; i<name.length;i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+     
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+             var counter = request.responseText;
+             var span = document.getElementById("count");
+             span.innerHTML = counter.toString();
+            }
+        }
+        
+    };
+    request.open('GET','http://saswat96.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
 };
