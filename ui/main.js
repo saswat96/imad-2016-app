8870411abfdb492708aeda638d8b563eae6812c6
Saswat Panigrahi <saswat96@gmail.com>
2016-11-08 17:36:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,7 @@ button.onclick = function() {
     request.send(null);
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_button');
 submit.onclick = function(){
      var request = new XMLHttpRequest();
@@ -43,6 +42,8 @@ submit.onclick = function(){
         }
         
     };
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://saswat96.imad.hasura-app.io/submit-name?name' + name, true);
     request.send(null);
     
