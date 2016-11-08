25a231f4d173ba3ce50abbb8fae0b1da6a82f5d0
Saswat Panigrahi <saswat96@gmail.com>
2016-11-08 17:31:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,13 @@ button.onclick = function() {
     request.onreadystatechange = function() {
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-            
+             var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i= 0; i<name.length;i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
             }
         }
         
@@ -21,19 +27,24 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_button');
 submit.onclick = function(){
-    
-     
-    var request = new XMLHttpRequest();
+     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-             var counter = request.responseText;
-             var span = document.getElementById("count");
-             span.innerHTML = counter.toString();
+             var names = request.responseText;
+             names=JSON.parse(names); 
+    var list = '';
+    for(var i= 0; i<name.length;i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
             }
         }
         
     };
-    request.open('GET','http://saswat96.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.open('GET','http://saswat96.imad.hasura-app.io/submit-name?name' + name, true);
     request.send(null);
+    
+   
 };
